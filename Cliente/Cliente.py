68249be14b7d3d58c6cd4f629690b147344405cf
cesarlongart101 <cesarlongart101@gmail.com>
2023-09-05 21:45:25 +0200

    imprimir precios diarios

--- Cliente/Cliente.py
+++ Cliente/Cliente.py
@@ -3,9 +3,17 @@ import requests
 url = 'http://localhost:4000/obtener_datos_ano'
 response = requests.get(url)
 
-# if response.status_code == 200:
-#     data = response.json() 
-#     print(data) 
+if response.status_code == 200:
+    data = response.json() 
+    for dict in data:
+        print(dict["Closing Price"])
+
+
+
+
+
+
+
 # else:
 #     print('Error al hacer la solicitud')
 
